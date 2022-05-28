0a9f2572bb47f30b84c329b6a7f2cc30dc3ebd4c
Mikołaj Olejnik <olejnikmikolaj@gmail.com>
2022-05-28 02:15:33 +0200

    added opening video in new tab

--- client/src/components/Video/Video.tsx
+++ client/src/components/Video/Video.tsx
@@ -1,7 +1,9 @@
 import { Col } from "reactstrap";
 import { MdPerson, MdThumbUp } from 'react-icons/md';
-import HeartButton from "../common/HeartButton";
 import { useState } from "react";
+
+import videoApps from "../../VideoApps";
+import HeartButton from "../common/HeartButton";
 import './Video.scss'
 
 export interface IVideoProps {
@@ -11,15 +13,26 @@ export interface IVideoProps {
     likeCount: number;
     addDate: string;
     liked: boolean;
+    urlId: string;
+    serviceName: string;
 }
 
-export default function Video ({thumbnail, title, viewCount, likeCount, addDate, liked}: IVideoProps) {
+export default function Video ({thumbnail, title, viewCount, likeCount, addDate, liked, urlId, serviceName}: IVideoProps) {
     const [isFavourite, setIsFavourite] = useState(liked);
+
+    function createVideoLink(): string {
+        for (const app of videoApps) {
+            if (app.name === serviceName) {
+                return app.videoUrl + urlId;
+            }
+        }
+        return ''
+    }
 
     return (
     <Col xs={12} sm={6} md={4} lg={4}>
         <div className='video-listing-item'>
-            <div className='thumbnail'>
+            <div className='thumbnail' onClick={() => window.open(createVideoLink(), '_blank')}>
                 <img src={thumbnail} alt={title} />
             </div>
             <div className='title'>
